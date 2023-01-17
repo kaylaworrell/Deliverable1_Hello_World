3052e1c845f7f0c0b1cd4d32c570c532b02f5573
Kayla Worrell <Kayla Worrell@KAYLAS-LAPTOP>
2023-01-17 16:10:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Deliverable1_Hello_World
+{
+       internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Solution (1): Hello World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World! (2)";
+            Console.WriteLine("Soluion (2): " + stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + username);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter the user's age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + age);
+        }
+    }
+}
